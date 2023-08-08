ca02c2e386ceb7a07adcf6443d85d8e2173a2740
Jade <jadecdrb@gmail.com>
2023-08-08 15:45:07 -0400

    added an index and show route and passed data

--- MOD3/Week-1/Day-2-Tue/food/controllers/fruitController.js
+++ MOD3/Week-1/Day-2-Tue/food/controllers/fruitController.js
@@ -3,12 +3,12 @@ const fruits = require('../models/fruits')
 
 // The callback function from the "index" route
 function index(req, res) {
-    res.render('fruits/Index')
+    res.render('fruits/Index', { fruits })
 }
 
 // The callback function from the "show" route
 function show(req, res) {
-    res.render('fruits/Show')
+    res.render('fruits/Show', { fruit: fruits[req.params.indexOfFruit] })
 }
 
 module.exports = {
--- MOD3/Week-1/Day-2-Tue/food/views/fruits/Index.jsx
+++ MOD3/Week-1/Day-2-Tue/food/views/fruits/Index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Index() {
+function Index(props) {
     return (
         <div>
             <h1>Index</h1>
+            {props.fruits.map((fruit, i) => <p key={i}>{fruit.name}</p>)}
         </div>
     )
 }
