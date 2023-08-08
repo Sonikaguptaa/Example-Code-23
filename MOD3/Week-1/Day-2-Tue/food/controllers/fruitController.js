a63fc2805652a4d9f4a84333db0b2a6251ffa0d3
Jade <jadecdrb@gmail.com>
2023-08-08 15:32:22 -0400

    added some views

--- MOD3/Week-1/Day-2-Tue/food/controllers/fruitController.js
+++ MOD3/Week-1/Day-2-Tue/food/controllers/fruitController.js
@@ -3,12 +3,12 @@ const fruits = require('../models/fruits')
 
 // The callback function from the "index" route
 function index(req, res) {
-    res.send(fruits)
+    res.render('fruits/Index')
 }
 
 // The callback function from the "show" route
 function show(req, res) {
-    res.send(fruits[req.params.indexOfFruit])
+    res.render('fruits/Show')
 }
 
 module.exports = {
