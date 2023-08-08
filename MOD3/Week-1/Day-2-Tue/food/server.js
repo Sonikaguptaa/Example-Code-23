2ba7b0ccbaf921350170dfe245f941867c6b3c7a
Jade <jadecdrb@gmail.com>
2023-08-08 15:20:35 -0400

    added view engine configuration

--- MOD3/Week-1/Day-2-Tue/food/server.js
+++ MOD3/Week-1/Day-2-Tue/food/server.js
@@ -7,6 +7,15 @@ const PORT = 8080
 // Bring in our routes
 const fruitRoutes = require('./routes/fruitRoutes')
 
+// Load the create engine function
+const jsxEngine = require('jsx-view-engine')
+
+// Configure the view engine and look for files ending in jsx
+app.set('view engine', 'jsx')
+
+// Createthe engine and accept files ending in jsx
+app.engine('jsx', jsxEngine())
+
 // Connect our fruit routes to our express app
 app.use('/fruits', fruitRoutes)
 
