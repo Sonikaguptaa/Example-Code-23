9908a5f7c312782d7cee9a1ecfa6679301facb6e
Jade <jadecdrb@gmail.com>
2023-08-11 13:12:09 -0400

    added logic for create and find

--- MOD3/Week-1/Day-4-Fri/food-mvc-db/controllers/fruitController.js
+++ MOD3/Week-1/Day-4-Fri/food-mvc-db/controllers/fruitController.js
@@ -1,14 +1,22 @@
 // Bring in our fruit data
-const fruits = require('../models/fruits')
+let fruits = require('../models/fruits')
 
 // Load the Fruit model so we can interact with the collection
 const Fruit = require('../models/Fruit')
 
 // GET /fruits
 module.exports.index = async (req, res) => {
-    const result = await Fruit.find()
-    console.log(result)
-    res.render('fruits/Index', { fruits: result })
+    let fruits;
+    
+    try {
+        fruits = await Fruit.find()
+        console.log(fruits)
+    } catch(err) {
+        console.log('Failed to create a Fruit document: ', err)
+    }
+    
+    console.log(fruits)
+    res.render('fruits/Index', { fruits })
 }
 
 //  GET /fruits/:indexOfFruits
@@ -32,15 +40,24 @@ module.exports.edit = (req, res) => {
 }
 
 // POST /fruits
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
     console.log('POST /fruits')
-    console.log(req.body) // <-- should contain form data
+    console.log(req.body) 
+
     if (req.body.readyToEat === 'on') {
         req.body.readyToEat = true
     } else {
         req.body.readyToEat = false
     }
-    fruits.push(req.body)
+
+    try {
+        let fruit = await Fruit.create(req.body)
+        console.log(fruit)
+    } catch(err) {
+        console.log('Failed to create a Fruit document: ', err)
+    }
+
+    // fruits.push(req.body)
     res.redirect('/fruits')
 }
 
