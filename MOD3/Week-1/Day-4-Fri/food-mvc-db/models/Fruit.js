e5a0066af69127a7bb1c577af7ab05d9560c4593
Jade <jadecdrb@gmail.com>
2023-08-11 12:45:56 -0400

    updated the fruit schema

--- MOD3/Week-1/Day-4-Fri/food-mvc-db/models/Fruit.js
+++ MOD3/Week-1/Day-4-Fri/food-mvc-db/models/Fruit.js
@@ -2,8 +2,23 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+// this Schema acts as a blueprint for our data
 const fruitSchema = new Schema({
     name: {
-        type: String
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    readyToEat: {
+        type: Boolean,
+        required: true
     }
 })
+
+// 1st argument is the collection, 2nd is the Schema (blueprint) 
+const Fruit = mongoose.model('fruits')
+
+module.exports = Fruit
