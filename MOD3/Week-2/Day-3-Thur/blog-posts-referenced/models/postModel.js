6003aec15b2d698ef2b441b93f0587477581bfcf
Jade <jadecdrb@gmail.com>
2023-08-17 13:22:03 -0400

    added refence logic to schemas

--- MOD3/Week-2/Day-3-Thur/blog-posts-referenced/models/postModel.js
+++ MOD3/Week-2/Day-3-Thur/blog-posts-referenced/models/postModel.js
@@ -8,6 +8,12 @@ const postSchema = new Schema({
    body: { type: String },
    user: { type: String, default: 'Bob' },
    // comments field here
+   comments: [{
+      // an id referencing the comment document
+      type: mongoose.Types.ObjectId,
+      // search for it in the comments collection
+      ref: 'comments'
+   }]
 }, { timestamps: true })
 
 const Post = mongoose.model('posts', postSchema)
