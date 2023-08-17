31a341ab6122d3ae6ebe253eee120dca2bf7685e
Jade <jadecdrb@gmail.com>
2023-08-17 11:22:31 -0400

    added the comment schema

--- MOD3/Week-2/Day-3-Thur/blog-posts-embedded/models/postModel.js
+++ MOD3/Week-2/Day-3-Thur/blog-posts-embedded/models/postModel.js
@@ -3,11 +3,20 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+// const commentSchema = new Schema({
+//       text: { type: String },
+//       user: { type: String, default: 'Bob' }
+// })
+
 const postSchema = new Schema({
    subject: { type: String },
    body: { type: String },
    user: { type: String, default: 'Bob' },
    // comments field here
+   comments: [{
+      text: { type: String },
+      user: { type: String, default: 'Bob' }
+   }]
 }, { timestamps: true })
 
 const Post = mongoose.model('posts', postSchema)
